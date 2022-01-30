22c52584ce47f1a6683c78c9a919e6db67571298
cherry26nat <nat26arhe@gmail.com>
2022-01-29 19:52:43 -0500

    Refactoring code

--- js/utils.js
+++ js/utils.js
@@ -2,42 +2,42 @@ const elementListCard = document.querySelector("#pokemon-list");
 
 //*****FETCH API******
 const fetchApi = async (url) => {
-	const response = await fetch(url);
+  const response = await fetch(url);
 
-	if (!response.ok) return null;
+  if (!response.ok) return null;
 
-	const dataJson = await response.json();
-	return await dataJson;
+  const dataJson = await response.json();
+  return await dataJson;
 };
 
 const fetchPokemon = async (url) => {
-	const pokemonDetail = await fetchApi(url);
+  const pokemonDetail = await fetchApi(url);
 
-	if (!pokemonDetail) return null;
+  if (!pokemonDetail) return null;
 
-	const pokemonSpecies = await fetchApi(pokemonDetail.species.url);
+  const pokemonSpecies = await fetchApi(pokemonDetail.species.url);
 
-	const pokemonEvolutions = await fetchApi(pokemonSpecies.evolution_chain.url);
+  const pokemonEvolutions = await fetchApi(pokemonSpecies.evolution_chain.url);
 
-	return {
-		id: pokemonDetail.id,
-		name: pokemonDetail.name,
-		detail: pokemonDetail,
-		species: pokemonSpecies,
-		evolutions: pokemonEvolutions,
-	};
+  return {
+    id: pokemonDetail.id,
+    name: pokemonDetail.name,
+    detail: pokemonDetail,
+    species: pokemonSpecies,
+    evolutions: pokemonEvolutions,
+  };
 };
 
 const renderPokemonInfo = (pokemon) => {
-	console.log(pokemon);
+  console.log(pokemon);
 
-	elementListCard.innerHTML += `<a  href="info.html?id=${pokemon.id}" >
+  elementListCard.innerHTML += `<a  href="info.html?id=${pokemon.id}" >
 	<div class="card">
 	<div class="pokemon-img">
 		<img src="${pokemon.detail.sprites.front_default}" alt="" srcset="" />
 	</div>
 	<div class="pokemon-info">
-	<h2>${pokemon.name}</h2>
+	<h3>${pokemon.name}</h3>
 	<div class="wrapper-species">
 	${mapRenderPokemonType(pokemon.detail.types)}
 	</div>
@@ -47,9 +47,9 @@ const renderPokemonInfo = (pokemon) => {
 };
 
 const mapRenderPokemonType = (types) =>
-	types.map(
-		(pokemontype) =>
-			`<div class="${colorType(pokemontype.type.name)}">${pokemontype.type.name}</div>`
-	);
+  types.map(
+    (pokemontype) => `<div class="pokemon-type">${pokemontype.type.name}</div>`
+  );
 
-const colorType = (type) => colorsByType[type];
+//Return color pokemon type
+const colorType = (typePokemon) => dataByType[typePokemon].class;
